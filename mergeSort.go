915aaafbbd4e2732927e5943b2c33533a76be82c
gundamzaku <gundamzaku@163.com>
2018-02-26 17:39:12 +0800

    新加自下而上的归并排序

--- mergeSort.go
+++ mergeSort.go
@@ -8,11 +8,12 @@
 	"T","E","X","A","M","P","L","E"
 	……后面类推
  */
- 
+
 package main
 
 import (
 	"elementarySort"
+	"fmt"
 )
 
 type MergeSort struct {
@@ -50,6 +51,18 @@ func (st *MergeSort) SortInit(a []string){
 	st.Sort(a,0,len(a)-1)
 
 }
+
+func (st *MergeSort) SortBu(a []string){
+	n := len(a)
+	for sz := 1; sz < n; sz = sz + sz {
+		fmt.Println("sz=>",sz)
+		for lo := 0; lo < n-sz; lo += sz + sz {
+			fmt.Println("lo=>",lo)
+			st.Merge(a,lo,lo+sz-1,Min(lo+sz+sz-1,n-1))
+		}
+	}
+}
+
 func (st *MergeSort) Sort(a []string,lo int,hi int){
 	if(hi<=lo){
 		return
@@ -61,10 +74,24 @@ func (st *MergeSort) Sort(a []string,lo int,hi int){
 	st.Merge(a,lo,mid,hi)
 }
 
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func main()  {
 	element := MergeSort{}
 	var a []string
+	//a = []string{"E","E","G","M","R","A","C","E","R","T"}
 	a = []string{"M","E","R","G","E","S","O","R","T","E","X","A","M","P","L","E"};
+
+	//自上而下的归并排序
 	element.SortInit(a)
+	element.Show(a)
+
+	//自下而上的归并排序
+	element.SortBu(a)
 	element.Show(a)
 }
